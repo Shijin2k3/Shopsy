51ed1d3c0e440a323bab370d9766488228dd92c0
Shijin2k3 <shijinkumar2003@gmail.com>
2025-03-11 20:58:18 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+function App() {
+
+
+  return (
+    <>
+      <div className="text-3xl text-red-500 font-bold">
+        <h1>Shopsy</h1>
+      </div>
+     
+    </>
+  )
+}
+
+export default App
