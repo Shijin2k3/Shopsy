c8995c69f5737e1bde85cbe479a41e0f26950bde
Shijin2k3 <shijinkumar2003@gmail.com>
2025-03-13 20:11:09 +0530

    initialized Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
+import Banner from "./components/Banner"
+import Category from "./components/Category"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import Home from "./components/Home"
+import Insta from "./components/Insta"
+import Products from "./components/Products"
+import Reviews from "./components/Reviews"
+import Services from "./components/Services"
+import Types from "./components/Types"
 
 function App() {
 
 
   return (
     <>
-      <div className="text-3xl text-red-500 font-bold">
-        <h1>Shopsy</h1>
-      </div>
-     
+     <Header />
+     <Home />
+     <Category />
+     <Types />
+     <Services />
+     <Products />
+     <Banner />
+     <Reviews />
+     <Insta />
+     <Footer />
     </>
   )
 }
